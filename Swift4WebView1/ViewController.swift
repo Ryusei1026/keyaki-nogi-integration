dc85dac91a8d93d678fd6052f6c9e2568cbf29dc
Ryusei Takano <s1250034@gmail.com>
2018-10-28 12:04:56 +0900

    Initial Commit

---
+++ Swift4WebView1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift4WebView1
+//
+//  Created by 高野隆正 on 2018/10/28.
+//  Copyright © 2018 高野隆正. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
